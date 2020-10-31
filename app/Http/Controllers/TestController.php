adfe1333ab70bdd27828fd25e799a0e940bc2cb0
スタディプレイス.me <studyplace.me_19@sutadipureisumenoMacBook-Air.local>
2020-10-31 13:30:34 +0900

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    public function index(){
+        $items = DB::select('select * from user');
+        return view('test',['items' => $items]);
+    }
+
+    public function create(){
+        $items = DB::select('select * from user');
+        return view('test',['items' => $items]);
+    }
+}
